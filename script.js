c76c0b57a5b95a01031869bc4472a62ef091706e
jestty <121748586+jestty@users.noreply.github.com>
2023-02-25 22:22:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-*/
-let seccretNumber, score, highScore;
 //tao ham toi uu code
+let seccretNumber, score, highScore;
+seccretNumber = Math.trunc(Math.random() * 20) + 1;
+score = 20;
+highScore = 0;
 const init = function () {
-  seccretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  highScore = 0;
+  seccretNumber = Math.trunc(Math.random() * 20) + 1;
   displayMessage('Start guessing...');
   displayNumber('?');
   displayScore(score);
@@ -31,11 +24,11 @@ const displayNumber = function (message) {
 const displayScore = function (message) {
   document.querySelector('.score').textContent = message;
 };
+
 //thao tac voi nut again trang thai ban dau ma giu nguyen high score
 document.querySelector('.again').addEventListener('click', init);
 
 //thao tac even voi nut check
-init();
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -49,10 +42,8 @@ document.querySelector('.check').addEventListener('click', function () {
   else if (guess === seccretNumber) {
     // document.querySelector('.message').textContent = '🎉 Correct Number!';
     displayMessage('🎉 Correct Number!');
-
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    // document.querySelector('.number').textContent = seccretNumber;
     displayNumber(seccretNumber);
 
     if (score > highScore) {
@@ -62,41 +53,15 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   //when wrong
   else if (guess !== seccretNumber) {
-    //truong hop seccretNumber khong xac dinh undefined thi xay ra loi bi thua luon
     if (score > 1) {
-      // document.querySelector('.message').textContent =
-      // guess > seccretNumber ? '📈 To Hight!' : '📉 To Low!';
       displayMessage(guess > seccretNumber ? '📈 To Hight!' : '📉 To Low!');
       score--;
-      // document.querySelector('.score').textContent = score;
       displayScore(score);
     } else {
-      // document.querySelector('.message').textContent = '🎆 You lost the game!';
       displayMessage('🎆 You lost the game!');
-      // document.querySelector('.score').textContent = 0;
       displayScore(0);
     }
   }
 });
-//when guess to hight
-// else if (guess > seccretNumber) {
-//   if (score > 1) {
-//     document.querySelector('.message').textContent = '📈 To Hight!';
-//     score--;
-//     document.querySelector('.score').textContent = score;
-//   } else {
-//     document.querySelector('.message').textContent = '🎆 You lost the game!';
-//     document.querySelector('.score').textContent = 0;
-//   }
-// }
-//when guess to low
-// else if (guess < seccretNumber) {
-//   if (score > 1) {
-//     document.querySelector('.message').textContent = '📉 To Low!';
-//     score--;
-//     document.querySelector('.score').textContent = score;
-//   } else {
-//     document.querySelector('.message').textContent = '🎆 You lost the game!';
-//     document.querySelector('.score').textContent = 0;
-//   }
-// }
+
+
